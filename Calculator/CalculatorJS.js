1b97cd3e2da46f22355d4af9553f9b80e9ac2f76
francisallin <lamtszto1997@gmail.com>
2023-05-01 19:07:33 -0400

    add css to form & set BVD according to radio btn

--- Calculator/CalculatorJS.js
+++ Calculator/CalculatorJS.js
@@ -1,42 +1,59 @@
 $(document).ready(function() {
+    //block custom field at first
     $('#OriBVD').prop("disabled", true);
     $('#NewBVD').prop("disabled", true);
     $('#Modes').change(function(){
-        //enable the custom field if custom mode is checked
-        if($('#Custom').is(":checked")) {
+        //set BVD form value according to mode selected
+        if ($('#SpecCL').is(":checked")){
+            $('#OriBVD').val("12");
+            $('#NewBVD').val("0");
+            $('#OriBVD').prop("disabled", true);
+            $('#NewBVD').prop("disabled", true);        
+        }
+        else if ($('#CLSpec').is(":checked")){
+            $('#OriBVD').val("0");
+            $('#NewBVD').val("12");
+            $('#OriBVD').prop("disabled", true);
+            $('#NewBVD').prop("disabled", true);            
+        }
+        else if($('#Custom').is(":checked")) {
             $('#OriBVD').prop("disabled", false);
             $('#NewBVD').prop("disabled", false);
         }
+        Calculate();
     })
-    $('#OriginalRx').change(function(){
-        //setting variable for BVD
+    $('#OriginalRx').change(Calculate);
+    function Calculate(){
+        //set variable for BVD
         let oriBVD = $('#OriBVD').val();
         let newBVD = $('#NewBVD').val();
-        //setting variable for R & L meridians
-        let rMeridian1;
-        let rMeridian2;
-        let lMeridian1;
-        let lMeridian2;
-        if(!(oriBVD&&newBVD)){ //if the custom BVD is empty, default 12 mm to 0 mm
-            oriBVD = 12;
-            newBVD = 0;
-        }
+        //declare variable for R & L meridians
+        let rMeridian1, rMeridian2, lMeridian1, lMeridian2;
         //calculate R new Rx
         let oriRSph = parseFloat($('#OriRSph').val());
         let oriRCyl = parseFloat($('#OriRCyl').val());
-        rMeridian1 = oriRSph/(1-Math.abs((oriBVD-newBVD)/1000)*oriRSph);
-        rMeridian2 = (oriRSph+oriRCyl)/(1-Math.abs((oriBVD-newBVD)/1000)*(oriRSph+oriRCyl));
+        rMeridian1 = oriRSph/(1-(oriBVD-newBVD)/1000*oriRSph);
+        rMeridian2 = (oriRSph+oriRCyl)/(1-(oriBVD-newBVD)/1000*(oriRSph+oriRCyl));
         $('#NewRSph').val(rMeridian1.toFixed(2));
-        $('#NewRCyl').val((rMeridian2-rMeridian1).toFixed(2));
-        $('#NewRAxis').val($('#OriRAxis').val());
+        if($('#OriRCyl').val()==""||$('#OriRAxis').val()=="") {
+            $('#NewRCyl').val("");
+            $('#NewRAxis').val("");
+        } else {
+            $('#NewRCyl').val((rMeridian2-rMeridian1).toFixed(2));
+            $('#NewRAxis').val($('#OriRAxis').val());
+        }
         //Calculate L new Rx
         let oriLSph = parseFloat($('#OriLSph').val());
         let oriLCyl = parseFloat($('#OriLCyl').val());
-        lMeridian1 = oriLSph/(1-Math.abs((oriBVD-newBVD)/1000)*oriLSph);
-        lMeridian2 = (oriLSph+oriLCyl)/(1-Math.abs((oriBVD-newBVD)/1000)*(oriLSph+oriLCyl));
+        lMeridian1 = oriLSph/(1-(oriBVD-newBVD)/1000*oriLSph);
+        lMeridian2 = (oriLSph+oriLCyl)/(1-(oriBVD-newBVD)/1000*(oriLSph+oriLCyl));
         $('#NewLSph').val(lMeridian1.toFixed(2));
-        $('#NewLCyl').val((lMeridian2-lMeridian1).toFixed(2));
-        $('#NewLAxis').val($('#OriLAxis').val());
-
-    })
+        if($('#OriLCyl').val()==""||$('#OriLAxis').val()=="") {
+            $('#NewLCyl').val("");
+            $('#NewLAxis').val("");
+        } else {
+            $('#NewLCyl').val((lMeridian2-lMeridian1).toFixed(2));
+            $('#NewLAxis').val($('#OriLAxis').val());
+        }
+    }
 })
